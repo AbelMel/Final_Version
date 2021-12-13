45c7002a6e84df71c2653c2567f2b9d7a8a19c28
AbelMel <b00137882@mytudublin.ie>
2021-12-13 04:04:21 +0000

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,13 +8,18 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level1"); //play button loads the first level
+        SceneManager.LoadScene("Level1");
     }
 
     public void QuitGame()
     {
         Debug.Log("QUIT!"); //quit the game
         Application.Quit();
+    }
+
+    public void GitHub()
+    {
+        Application.OpenURL("https://github.com/AbelMel/Final_Version");
     }
 
 }
